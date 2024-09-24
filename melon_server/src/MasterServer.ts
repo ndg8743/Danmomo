ed098e4711d9eb643919afe071a8f6e2f269ba2b
CJ <iron768gamer@gmail.com>
2024-09-24 18:48:23 -0400

    feat: type config, add rest and socket config

--- melon_server/src/MasterServer.ts
+++ melon_server/src/MasterServer.ts
@@ -7,9 +7,10 @@ class MasterServer extends Server {
     constructor() {
         super('master', config);
 
-        const port = config.port;
+        const restPort = config.restPort;
+        const socketPort = config.socketPort;
 
-        console.log("[Melon] Server '" + this.serverId + "' starting on port " + port);
+        console.log("[Melon] Server '" + this.serverId + "' starting on REST port " + restPort + " socket port " + socketPort);
     }
 
 }
---
+++ melon_server/src/config/Config.ts
@@ -0,0 +1,6 @@
+export interface Config {
+
+    restPort: number;
+    socketPort: number;
+    
+}
