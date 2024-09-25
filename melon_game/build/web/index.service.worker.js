4d1bb1fd1addbbf461102a7e9f35990b52c02380
Nathan G <73437724+ndg8743@users.noreply.github.com>
2024-09-24 22:33:08 -0400

    lets try export again

--- melon_game/build/web/index.service.worker.js
+++ melon_game/build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1727230744|522352984';
+const CACHE_VERSION = '1727231545|1323935889';
 /** @type {string} */
 const CACHE_PREFIX = '🥝-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
