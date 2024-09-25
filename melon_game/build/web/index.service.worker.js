853cae5935744ed6d5b7e06844339cdb972e93dd
Nathan G <73437724+ndg8743@users.noreply.github.com>
2024-09-25 00:38:14 -0400

    uhhh

--- melon_game/build/web/index.service.worker.js
+++ melon_game/build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1727238309|34201411';
+const CACHE_VERSION = '1727239061|786220513';
 /** @type {string} */
 const CACHE_PREFIX = '🥝-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
