dd79a4c26cc3eb17c58b26150441067d8d5ce1a1
CJ <iron768gamer@gmail.com>
2024-09-28 15:23:42 -0400

    feat: server - add sockets

--- melon_server/src/MasterServer.ts
+++ melon_server/src/MasterServer.ts
@@ -1,6 +1,9 @@
 import config from '../config/config.json';
 
 import Server from './server/Server';
+import { Config } from './config/Config';
+import * as http from 'http';
+import { Socket } from 'socket.io';
 
 class MasterServer extends Server {
 
@@ -11,6 +14,32 @@ class MasterServer extends Server {
         const socketPort = config.socketPort;
 
         console.log("[Melon] Server '" + this.serverId + "' starting on REST port " + restPort + " socket port " + socketPort);
+
+        const io = this.createSocket(config, {
+            cors: {
+                origin: '*'
+            }
+        });
+
+        const server = io.listen(socketPort);
+
+        server.on('connection_error', (error: Error) => { 
+            console.error("[Melon] Connection error: ", error) 
+        });
+
+        server.on('connection', (socket: Socket) => {
+            this.onConnection(socket);
+        });
+    }
+
+    createSocket(config: Config, options: any) {
+        const server = http.createServer((req, res) => {});
+
+        return require('socket.io')(server, options);
+    }
+
+    onConnection(socket: Socket) {
+        console.log("[Melon] New connection: " + socket.id);
     }
 
 }
