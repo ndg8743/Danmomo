66e25d19c38a7c50332f6295a8600522c911ca54
CJ <iron768gamer@gmail.com>
2024-10-08 17:54:10 -0400

    fix: error handling for base handler

--- melon_server/src/handler/BaseHandler.ts
+++ melon_server/src/handler/BaseHandler.ts
@@ -9,6 +9,7 @@ export default class BaseHandler {
     public config: Config;
     public plugins?: PluginManager;
     public events: EventEmitter;
+    public logging: boolean;
 
     constructor(handlerId: string, users: [], config: Config) {
         this.handlerId = handlerId;
@@ -16,11 +17,12 @@ export default class BaseHandler {
         this.config = config;
 
         this.events = new EventEmitter({ captureRejections: true });
+        this.logging = config.logging;
 
         this.startPlugins(handlerId);
 
         this.events.on('error', (error: Error) => {
-            // TODO: handle errror!
+            this.error(error);
         })
     }
 
@@ -28,8 +30,29 @@ export default class BaseHandler {
         this.plugins = new PluginManager(this, directory);
     }
 
+    // TODO: fix user typing
     handle(message: { action: string, args: [] }, user: any) {
+        try {
+            if (this.logging) {
+                console.log(`[Melon](${this.handlerId}) Received: ${message.action} ${JSON.stringify(message.args)}`);
+            }
 
+            this.events.emit(message.action, message.args, user);
+
+            if (user.events) {
+                user.events.emit(message.action, message.args, user);
+            }
+        } catch (error) {
+            this.error(error);
+        }
+    }
+
+    close(user: any) {
+        delete this.users[user.socket.id];
+    }
+
+    error(error: any) {
+        console.error(`[Melon](${this.handlerId}) ERROR: ${error.stack}`)
     }
 
 }
--- melon_server/src/object/Config.ts
+++ melon_server/src/object/Config.ts
@@ -1,10 +1,15 @@
 export interface ServerConfig {
+
     port: number;
+
 }
 
 export interface Config {
+
     port: number;
+    logging: boolean;
     servers: {
         [key: string]: ServerConfig;
     };
+
 }
