5b96d565582c6c9ed1fbd993b93cafb6fedf55fd
CJ <iron768gamer@gmail.com>
2024-10-15 18:32:55 -0400

    feat: update server order and ports and expand a bit more on matchmaking

--- melon_server/src/plugin/game/Matchmaker.ts
+++ melon_server/src/plugin/game/Matchmaker.ts
@@ -1,11 +1,17 @@
+import QueuedMatch from "../../object/match/QueuedMatch";
 import User from "../../object/user/User";
+import GameServer from "../../server/impl/GameServer";
 import Server from "../../server/Server";
 import Plugin from "../Plugin";
 
 export default class MatchMaker extends Plugin {
 
-    constructor(server: Server) {
+    private gameServer: GameServer;
+
+    constructor(server: GameServer) {
         super(server);
+
+        this.gameServer = server;
 
         this.events = {
             'queueMatch': this.queueMatch
@@ -13,9 +19,9 @@ export default class MatchMaker extends Plugin {
     }
 
     async queueMatch(args: any, user: User) {
-        
-        
+        this.parseMatch(args, user);
 
+        user.send('message', { message: 'Match queued started...' });
     }
 
     parseMatch(args: any, user: User) {
@@ -23,7 +29,20 @@ export default class MatchMaker extends Plugin {
             user.send('error', { message: 'Invalid match details!' });
             return;
         }
+        
+        const queueSize = this.gameServer.queuedMatches.length;
 
+        if (queueSize > 1) {
+
+        }
+
+        const newMatch = new QueuedMatch(queueSize + 1, user, null);
+
+        this.gameServer.queuedMatches.push(newMatch);
+    }
+
+    startMatch() {
+        
     }
 
 }
--- melon_server/src/server/impl/GameServer.ts
+++ melon_server/src/server/impl/GameServer.ts
@@ -1,10 +1,18 @@
 import { Config } from "../../object/Config";
+import Match from "../../object/match/Match";
 import Server from "../Server";
+import QueuedMatch from '../../object/match/QueuedMatch';
 
 export default class GameServer extends Server {
 
+    public queuedMatches: QueuedMatch[];
+    public matches: Match[];
+
     constructor(serverId: string, config: Config) {
         super(serverId, config);
+
+        this.queuedMatches = [];
+        this.matches = [];
     }
 
 }
