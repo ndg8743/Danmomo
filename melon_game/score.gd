8be3ae375b5c802116886a84d7ee462433362fd0
Nathan Gopee <73437724+ndg8743@users.noreply.github.com>
2024-10-25 14:22:16 -0400

    Fixed score on main menu bug

--- melon_game/score.gd
+++ melon_game/score.gd
@@ -3,23 +3,24 @@ class_name Score
 
 var combo_counter := 1
 var score := 0
+var last_score := 0  # Store the score from the previous game
 var is_game_over := true
 
 @onready var fade : Sprite2D = $"/root/transition/fade"
 
 func _ready():
-	text = ""
+	text = ""  # Ensure score is blank on load
 
 func level_start():
-	$"../prev_score".text = text
-	text = ""
+	$"../prev_score".text = str(last_score)  # Show last score when starting a new level
+	text = ""  # Clear current score display
 	score = 0
 	is_game_over = false
 
 func end_combo():
 	combo_counter = 1
 
-func add(val:  int):
+func add(val: int):
 	if is_game_over:
 		return
 	combo_counter += 1
@@ -33,7 +34,9 @@ func game_over():
 	if is_game_over:
 		return
 	is_game_over = true
-	$"../prev_score".text = ""
-	
-func _process(_delta:float):
+	last_score = score  # Save the score for the next game
+	$"../prev_score".text = ""  # Clear score display on the main menu
+	text = ""  # Clear current score display
+
+func _process(_delta: float):
 	modulate.a = pow(1 - fade.modulate.a, 4)
